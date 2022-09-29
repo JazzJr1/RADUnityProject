d21de7df1827e96cd61cc39f1965625a21bfbdcd
STUDENT James Smith <James.Smith@students.ittralee.ie>
2022-09-29 10:49:02 +0100

    Continued with the movement of our character Signed-off-by: STUDENT James Smith <James.Smith@students.ittralee.ie>

--- The Cube Game/Assets/CubeControl.cs
+++ The Cube Game/Assets/CubeControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CubeControl : MonoBehaviour
 {
+    private float turningSpeed = 180;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +20,16 @@ public class CubeControl : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
             transform.position -= Vector3.forward * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
-            transform.position += Vector3.left * Time.deltaTime;
+            transform.position -= Vector3.right * Time.deltaTime;
         if (Input.GetKey(KeyCode.D))
+        { 
             transform.position += Vector3.right * Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space))
-            transform.position += Vector3.up * Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0, -30), 1);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.down,turningSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up,turningSpeed * Time.deltaTime);
 
     }
 }
