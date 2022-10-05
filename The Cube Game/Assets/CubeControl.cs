6876c2d2afced4de9808398e1000c99c4fa7d465
STUDENT James Smith <James.Smith@students.ittralee.ie>
2022-10-05 12:43:57 +0100

    Signed-off-by: STUDENT James Smith <James.Smith@students.ittralee.ie>

--- The Cube Game/Assets/CubeControl.cs
+++ The Cube Game/Assets/CubeControl.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class CubeControl : MonoBehaviour
 {
     private float turningSpeed = 180;
+    Rigidbody ourRigidBody;
+    private void OnCollisionEnter(Collision collision)
+    {
+      // collision.transform.position += Vector3.down;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,21 +20,30 @@ public class CubeControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ourRigidBody = GetComponent<Rigidbody>();
+
         if (Input.GetKey(KeyCode.W))
-            transform.position += Vector3.forward * Time.deltaTime;
+        {
+            transform.position += Vector3.forward * Time.deltaTime * 2;
+        }
         if (Input.GetKey(KeyCode.S))
-            transform.position -= Vector3.forward * Time.deltaTime;
+        {
+            transform.position -= Vector3.forward * Time.deltaTime * 2;
+        }
         if (Input.GetKey(KeyCode.A))
-            transform.position -= Vector3.right * Time.deltaTime;
+        {
+            transform.position -= Vector3.right * Time.deltaTime * 2;
+        }
         if (Input.GetKey(KeyCode.D))
         { 
-            transform.position += Vector3.right * Time.deltaTime;
-            transform.Rotate(new Vector3(0, 0, -30), 1);
+            transform.position += Vector3.right * Time.deltaTime * 2;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.Rotate(Vector3.down,turningSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.up,turningSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+            ourRigidBody.AddForce(Vector3.up*300);
 
-    }
+}
 }
